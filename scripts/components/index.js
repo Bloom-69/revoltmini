1d8b4031be13f3e4d70372884b1b1b0324480a76
Bloom <valenplayer3000@gmail.com>
2023-11-17 22:04:18 -0400

    upd: small changes/improvement to ui

--- scripts/components/index.js
+++ scripts/components/index.js
@@ -1,7 +1,9 @@
 import { Message } from "./message.js";
 import { Modal } from "./modal.js";
 import { Attachments } from "./attachments.js";
+import { Mdsymbols } from "./mdsymbols.js";
 
 customElements.define("message-renderer", Message);
 customElements.define("attachment-renderer", Attachments);
+customElements.define("material-symbols", Mdsymbols);
 customElements.define("custom-modal", Modal, { extends: "dialog" });
